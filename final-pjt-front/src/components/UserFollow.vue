d9cffa8e9669732ebf04e07bc0f45ad5c553eda2
kimseonghun96 <tjd951753@naver.com>
2022-11-21 02:31:14 +0900

    readme작성

--- final-pjt-front/src/components/UserFollow.vue
+++ final-pjt-front/src/components/UserFollow.vue
@@ -1,56 +1,47 @@
 <template>
   <span>
-    <!-- <span class="heart-btn" v-if="isUser">
+    <span class="heart-btn" v-if="isUser">
       <span class="content" @click="heart">
         <span class="heart" @click="follow"></span>
       </span>
-    </span> -->
+    </span>
   </span>
 </template>
 
 <script>
 // import jwt_decode from "jwt-decode";  -> 헤더에 토큰 담아 보내는 거
-// import axios from 'axios'
-// const API_URL = 'http://127.0.0.1:8000'
+import axios from 'axios'
+const API_URL = 'http://127.0.0.1:8000'
 export default {
   name: 'userFollow',
-  // data: function () {
-  //   return {
-  //     isActive:'heart-active',
-  //     isUser: null,
-  //   }
-  // },
-  // methods:{
-  //   setToken: function () {
-  //     const config = {
-  //       Authorization: `JWT ${this.$store.state.token}`
-  //     }
-  //     return config
-  //   },
-  //   follow: function () {
-  //     axios({
-  //         method: 'post',
-  //         url: `${API_URL}/accounts/follow/${this.$route.params.username}/`,
-  //         headers: this.setToken()
-  //       })
-  //       .then(res => {
-  //         this.$emit('count',res.data.count)
-  //       })
-  //       .catch(err => {
+  data: function () {
+    return {
+      // isActive:'heart-active',
+      isUser: null,
+    }
+  },
+  methods:{
+    setToken: function () {
+      const config = {
+        Authorization: `Token ${this.$store.state.token}`
+      }
+      return config
+    },
+    follow: function () {
+      axios({
+          method: 'post',
+          url: `${API_URL}/accounts/follow/${this.$route.params.username}/`,
+          headers: this.setToken()
+        })
+        .then(res => {
+          this.$emit('count',res.data.count)
+        })
+        .catch(err => {
 
-  //         console.log(err)
-  //       })
-  //   }, 
-//   },
-//   created: function (){
-//     const pagename = this.$route.params.username
-//       const username = username
-//     if (pagename === username){
-//       this.isUser = false
-//     } else {
-//       this.isUser =true
-//     }
-//   }
+          console.log(err)
+        })
+    }, 
+  },
 }
 </script>
 
--- final-pjt-front/src/views/MyPageView.vue
+++ final-pjt-front/src/views/MyPageView.vue
@@ -3,24 +3,17 @@
     <h1>mypage</h1>
     {{userInfo}}
     <p></p>
-    <!-- <router-link to="/userFollow">follow</router-link> -->
+
   </div>
 </template>
 
 <script>
-// import UserFollow from '@/components/UserFollow.vue'
+
 
 export default {
   name: 'MyPageView',
-  // components: { 
-  //   UserFollow,
-  // },
-  computed: {
-    userInfo() {
-      return this.$store.state.username
-    },
-  }
 }
+
 </script>
 
 <style lang="scss">
